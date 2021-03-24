cf6a194da359bc00c7f7c4cdf5ae63da922d988c
曹思奇 <375136282@qq.com>
2021-03-25 00:26:12 +0800

    ch1: change finished

--- kernel/main.c
+++ kernel/main.c
@@ -1,12 +1,32 @@
-#include "../libs/sbi.h"
+#include "defs.h"
 
-void main()
-{
-    SBI_PUTCHAR('H');
-    SBI_PUTCHAR('e');
-    SBI_PUTCHAR('l');
-    SBI_PUTCHAR('l');
-    SBI_PUTCHAR('o');
-    SBI_PUTCHAR('\n');
-    while(1) {}
+extern char s_text[];
+extern char e_text[];
+extern char s_rodata[];
+extern char e_rodata[];
+extern char s_data[];
+extern char e_data[];
+extern char s_bss[];
+extern char e_bss[];
+
+void clean_bss() {
+    char* p;
+    for(p = s_bss; p < e_bss; ++p)
+        *p = 0;
+}
+
+void main() {
+    clean_bss();
+    printf("\n");
+    printf("hello wrold!\n");
+    printf("stext: %p\n", s_text);
+    printf("etext: %p\n", e_text);
+    printf("sroda: %p\n", s_rodata);
+    printf("eroda: %p\n", e_rodata);
+    printf("sdata: %p\n", s_data);
+    printf("edata: %p\n", e_data);
+    printf("sbss : %p\n", s_bss);
+    printf("ebss : %p\n", e_bss);
+    printf("\n");
+    shutdown();
 }
